ae41524f15da2a008c02c4fe3e311dec88f9274e
DESKTOP-9RJJ0LV\Code Breaker <codebreakers786@gmail.com>
2021-06-17 08:37:00 +0500

    pc

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:metr_reading/models/report.dart';
 import 'package:metr_reading/screens/create_report_home.dart';
-import 'package:metr_reading/screens/report_viwer.dart';
 import 'package:metr_reading/services/could_firebase.dart';
+import 'package:metr_reading/utils/const.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,6 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ScrollController _scrollController = ScrollController();
   bool widgetVisible = false;
 
   void showWidget() {
@@ -59,34 +61,124 @@ class _HomePageState extends State<HomePage> {
               List<Report> reports = [];
               snapshot.data.docs.forEach(
                   (report) => reports.add(Report.fromMap(report.data())));
-              return ListView.builder(
+              return PageView.builder(
+                scrollDirection: Axis.vertical,
                 itemCount: reports.length,
                 itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      title: Text("Contact Name : " +
-                          reports[index].contactName.toString()),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Client : " + reports[index].client.toString()),
-                          Text("Site : " + reports[index].site.toString()),
-                          Text("Building : " +
-                              reports[index].building.toString()),
-                          Text("Carried Out By : " +
-                              reports[index].carriedoutonbehalfof.toString()),
-                          Text("Total Meters : " +
-                              reports[index].meters.length.toString()),
-                        ],
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            buildInfoRow(
+                              title: 'Contact Name:',
+                              info: reports[index].contactName,
+                            ),
+                            buildInfoRow(
+                              title: 'Client:',
+                              info: reports[index].client,
+                            ),
+                            buildInfoRow(
+                              title: 'Site:',
+                              info: reports[index].site,
+                            ),
+                            buildInfoRow(
+                              title: 'Building',
+                              info: reports[index].building,
+                            ),
+                            buildInfoRow(
+                              title: 'Carried out on: ',
+                              info: df
+                                  .format(reports[index].dateSurveyCarriedOut),
+                            ),
+                            buildInfoRow(
+                              title: 'Carried out on behalf of:',
+                              info: reports[index].carriedoutonbehalfof,
+                            ),
+                            reports[index].meters.length != 0
+                                ? ListView.separated(
+                                    physics: NeverScrollableScrollPhysics(),
+                                    shrinkWrap: true,
+                                    controller: _scrollController,
+                                    scrollDirection: Axis.vertical,
+                                    itemCount: reports[index].meters.length,
+                                    itemBuilder: (context, index) {
+                                      return Slidable(
+                                        actionPane: SlidableDrawerActionPane(),
+                                        //  actionExtentRatio: 0.25,
+                                        child: Container(
+                                          color: Colors.green.withOpacity(.4),
+                                          child: ListTile(
+                                            leading: CircleAvatar(
+                                              backgroundColor: Colors.white,
+                                              backgroundImage: AssetImage(
+                                                'assets/meter.png',
+                                              ),
+                                              foregroundColor: Colors.white,
+                                            ),
+                                            title: Text('Tile no $index'),
+                                            subtitle:
+                                                Text('SlidableDrawerDelegate'),
+                                          ),
+                                        ),
+                                        actions: <Widget>[
+                                          IconSlideAction(
+                                            caption: 'Archive',
+                                            color: Colors.blue,
+                                            icon: Icons.archive,
+                                            onTap: () => null,
+                                          ),
+                                          IconSlideAction(
+                                            caption: 'Share',
+                                            color: Colors.indigo,
+                                            icon: Icons.share,
+                                            onTap: () => null,
+                                          ),
+                                        ],
+                                        secondaryActions: <Widget>[
+                                          IconSlideAction(
+                                            caption: 'More',
+                                            color: Colors.black45,
+                                            icon: Icons.more_horiz,
+                                            onTap: () => null,
+                                          ),
+                                          IconSlideAction(
+                                            caption: 'Delete',
+                                            color: Colors.red,
+                                            icon: Icons.delete,
+                                            onTap: () {
+                                              reports[index]
+                                                  .meters
+                                                  .removeAt(index);
+                                              setState(() {});
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                    separatorBuilder: (context, index) {
+                                      return Divider(
+                                        height: 1,
+                                      );
+                                    },
+                                  )
+                                : Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 30),
+                                    child: Text(
+                                      "No Meter added yet ",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.grey),
+                                    ),
+                                  )
+                          ],
+                        ),
                       ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ReportViwer(
-                                      report: reports[index],
-                                    )));
-                      },
                     ),
                   );
                 },
@@ -104,6 +196,40 @@ class _HomePageState extends State<HomePage> {
             ),
           );
         },
+      ),
+    );
+  }
+
+  buildTableCellRow({String title, String info}) {
+    return TableRow(children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          title,
+          textScaleFactor: 1.5,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(info, textScaleFactor: 1.5),
+      ),
+    ]);
+  }
+
+  buildInfoRow({String title, String info}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          Text(info,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey)),
+        ],
       ),
     );
   }
--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,6 +1,9 @@
+import 'package:intl/intl.dart';
+
 const CLOUD_REPORTS = 'reports';
 const METER = 'meters';
 const MANUFACTURS = 'manufacturers';
 const METER_MODELS = 'meter_models';
 const PARENT_METERS = 'parent_meters';
 const FLOORS = 'floors';
+  final df = DateFormat('dd-MM-yyyy');
